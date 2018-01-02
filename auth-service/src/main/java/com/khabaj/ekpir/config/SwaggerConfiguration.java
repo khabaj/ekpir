1e357cec2af8bada32e03f73f300db1d46b332a9
khabaj <habajkrystian@gmail.com>
2018-01-02 10:05:32 +0100

    gateway service - centralized swagger docs.

--- auth-service/src/main/java/com/khabaj/ekpir/config/SwaggerConfiguration.java
+++ auth-service/src/main/java/com/khabaj/ekpir/config/SwaggerConfiguration.java
@@ -1,5 +1,8 @@
 package com.khabaj.ekpir.config;
 
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -10,6 +13,8 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Collections;
 
 @Configuration
@@ -17,7 +22,7 @@ import java.util.Collections;
 public class SwaggerConfiguration {
 
     @Bean
-    public Docket swaggerApi() {
+    public Docket swaggerApi() throws IOException, XmlPullParserException {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.khabaj.ekpir.controllers"))
@@ -26,14 +31,20 @@ public class SwaggerConfiguration {
                 .apiInfo(apiInfo());
     }
 
-    ApiInfo apiInfo() {
-        return new ApiInfo("e-kpir API",
-                "Api Documentation",
-                "",
+    private ApiInfo apiInfo() throws IOException, XmlPullParserException {
+        return new ApiInfo("Authentication Service API documentation",
+                "Authentication Service API documentation",
+                readApiVersion(),
                 "",
                 new Contact("Krystian Habaj", "", ""),
                 "",
                 "",
                 Collections.emptyList());
+    }
+
+    private String readApiVersion() throws IOException, XmlPullParserException {
+        MavenXpp3Reader reader = new MavenXpp3Reader();
+        Model model = reader.read(new FileReader("pom.xml"));
+        return model.getVersion();
     }
 }
